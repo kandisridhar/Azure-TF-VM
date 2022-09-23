c9480be82a46a4122ec4aeebd1e3a698f4b566e5
kandisridhar <53901088+kandisridhar@users.noreply.github.com>
2022-09-23 12:15:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+data "azurerm_resource_group" "rg"{
+  name = "ranjith"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+  name                 = "mySubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
